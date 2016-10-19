480021433dafde0454d83e44d5fb0caa8fc3e82f
Justin DuJardin <justin@dujardinconsulting.com>
2016-10-19 16:42:57 -0700

    feat(rpg-sprite): refactor to be reactive and use onPush change detection

--- src/app/components/rpg/rpgSprite.ts
+++ src/app/components/rpg/rpgSprite.ts
@@ -13,69 +13,81 @@
  See the License for the specific language governing permissions and
  limitations under the License.
  */
-import {Component} from '@angular/core';
+import {Component, Input, ChangeDetectionStrategy} from '@angular/core';
 import {RPGGame} from '../../services/index';
-import {GameWorld} from '../../services/gameWorld';
 import {SpriteRender} from '../../services/spriteRender';
+import {Observable, ReplaySubject, BehaviorSubject} from 'rxjs';
+import {DomSanitizer, SafeResourceUrl} from '@angular/platform-browser';
 
 @Component({
   selector: 'rpg-sprite',
-  inputs: ['name', 'frame', 'width', 'height'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
   host: {
-    '[style.width]': 'width',
-    '[style.height]': 'height'
+    '[style.width]': 'width$ | async',
+    '[style.height]': 'height$ | async'
   },
-  template: `<img [src]="dataUrl">`
+  template: `<img [src]="dataUrl$ | async">`
 })
 export class RPGSprite {
 
   static INVALID_IMAGE: string = 'images/a/blank.gif';
 
-  dataUrl: string = '';
-  width: string = '64';
-  height: string = '64';
+  private _dataUrl$ = new ReplaySubject<SafeResourceUrl>(1);
+  dataUrl$: Observable<SafeResourceUrl> = this._dataUrl$;
+  private _width$ = new BehaviorSubject<string>('64');
+  width$: Observable<string> = this._width$;
+  private _height$ = new BehaviorSubject<string>('64');
+  height$: Observable<string> = this._height$;
+  private _name$ = new BehaviorSubject<string>(RPGSprite.INVALID_IMAGE);
+  name$: Observable<string> = this._name$;
+  private _frame$ = new BehaviorSubject<number>(0);
+  frame$: Observable<number> = this._frame$;
 
-  private _frame: number = 0;
+  @Input()
+  set width(value: string) {
+    this._width$.next(value);
+  }
+
+  @Input()
+  set height(value: string) {
+    this._height$.next(value);
+  }
+
+  @Input()
   set frame(value: number) {
-    if (this._frame !== value) {
-      this._frame = value;
+    if (this._frame$.value !== value) {
+      this._frame$.next(value);
       this._get();
     }
   }
 
-  get frame(): number {
-    return this._frame;
-  }
 
-  private _name: string = RPGSprite.INVALID_IMAGE;
+  @Input()
   set name(value: string) {
     if (!value) {
-      this._name = RPGSprite.INVALID_IMAGE;
+      this._name$.next(RPGSprite.INVALID_IMAGE);
       return;
     }
-    this._name = value;
+    this._name$.next(value);
     this._get();
   }
 
-  get name(): string {
-    return this._name;
-  }
-
   constructor(private game: RPGGame,
-              private world: GameWorld,
+              private sanitizer: DomSanitizer,
               private renderer: SpriteRender) {
   }
 
   private _get() {
-    this.renderer.getSingleSprite(this._name, this._frame).then((sprite: HTMLImageElement) => {
+    this.renderer.getSingleSprite(this._name$.value, this._frame$.value).then((sprite: HTMLImageElement) => {
       // Get the context for drawing
-      var width: number = parseInt(this.width);
-      var height: number = parseInt(this.height);
+      const width: number = parseInt(this._width$.value);
+      const height: number = parseInt(this._height$.value);
 
-      var renderContext: any = this.game.getRenderContext(width, height);
+      const renderContext: any = this.game.getRenderContext(width, height);
       renderContext.clearRect(0, 0, width, height);
       renderContext.drawImage(sprite, 0, 0, width, height);
-      this.dataUrl = this.game.releaseRenderContext();
+      const imageDataUrl = this.sanitizer.bypassSecurityTrustResourceUrl(this.game.releaseRenderContext());
+      this._dataUrl$.next(imageDataUrl);
     });
 
   }
