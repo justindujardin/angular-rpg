e5c2b504f485e76ddb5c48fa0d487aedbe6c5aa5
justindujardin <justin@dujardinconsulting.com>
2020-12-12 15:07:28 -0800

    chore: remove more unused files

--- src/sw.browser.ts
+++
@@ -1,5 +0,0 @@
-import * as OfflinePluginRuntime from 'offline-plugin/runtime';
-OfflinePluginRuntime.install({
-  onUpdateReady: () => OfflinePluginRuntime.applyUpdate(),
-  onUpdated: () => location.reload(),
-});
