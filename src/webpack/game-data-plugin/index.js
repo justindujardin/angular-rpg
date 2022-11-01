5560c2d3772c6449866e22f3c73cea554b453ab7
Justin DuJardin <justin@dujardinconsulting.com>
2022-10-31 17:58:24 -0700

    perf(ng): only write tiled data when source file hashes change
     - the Tiled game data step is kind of slow to run for every source file change in the codebase.
     - keep a hash of the listed files that game data comes from, and only run the data generation script when they change.

--- src/webpack/game-data-plugin/index.js
+++ src/webpack/game-data-plugin/index.js
@@ -3,16 +3,61 @@
 const { exec } = require("child_process");
 const webpack = require("webpack");
 const { cwd } = require("process");
-const { readFileSync, writeFileSync } = require("fs");
+const { readFileSync, writeFileSync, createReadStream } = require("fs");
+const { createHash } = require("crypto");
+
+const GAME_DATA_COMMAND =
+  "npx ts-node --transpile-only --project src/tsconfig.write-game-data.json src/write-game-data.ts";
 
 const PLUGIN_NAME = "write-tiled-game-data";
 
 /** Tiled Editor project JSON file */
 const PROJECT_FILE = "angular-rpg.tiled-project";
 
+/** Game data is only written when these files change */
+const SOURCE_FILES = [
+  "./src/app/models/game-data/armors.ts",
+  "./src/app/models/game-data/fixed-encounters.ts",
+  "./src/app/models/game-data/items.ts",
+  "./src/app/models/game-data/magic.ts",
+  "./src/app/models/game-data/random-encounters.ts",
+  "./src/app/models/game-data/weapons.ts",
+];
+
+const SEEN_HASHES = {};
+
+/** Hash the source files used in write-game-data.ts and only run when they change */
+function shouldWriteData() {
+  function checksumFile(filename) {
+    return new Promise((resolve, reject) => {
+      const hash = createHash("sha1");
+      const stream = createReadStream(filename);
+      stream.on("error", (err) => reject(err));
+      stream.on("data", (chunk) => hash.update(chunk));
+      stream.on("end", () => resolve(hash.digest("hex")));
+    });
+  }
+  return Promise.all(SOURCE_FILES.map((filename) => checksumFile(filename))).then(
+    (hashes) => {
+      let should = false;
+      SOURCE_FILES.forEach((filename, index) => {
+        const hash = hashes[index];
+        if (!SEEN_HASHES[filename]) {
+          SEEN_HASHES[filename] = hash;
+          should = true;
+        }
+        if (SEEN_HASHES[filename] !== hash) {
+          should = true;
+        }
+        SEEN_HASHES[filename] = hash;
+      });
+      return should;
+    }
+  );
+}
+
 module.exports = class GameDataPlugin {
   apply(compiler) {
-    var self = this;
     compiler.hooks.thisCompilation.tap(PLUGIN_NAME, (compilation) => {
       compilation.hooks.processAssets.tapAsync(
         {
@@ -20,12 +65,14 @@ module.exports = class GameDataPlugin {
           stage: webpack.Compilation.PROCESS_ASSETS_STAGE_ADDITIONAL,
         },
         (assets, callback) => {
-          console.log("Writing game data files...");
-          console.log("CWD = " + cwd());
-
-          exec(
-            "npx ts-node --project src/tsconfig.write-game-data.json src/write-game-data.ts",
-            (error, stdout, stderr) => {
+          shouldWriteData().then((should) => {
+            if (!should) {
+              console.log("Skipping Tiled write because source files unchanged");
+              callback();
+              return;
+            }
+            console.log("Writing Tiled data files");
+            exec(GAME_DATA_COMMAND, (error, stdout, stderr) => {
               if (error) {
                 console.log(`error: ${error.message}`);
                 callback();
@@ -68,8 +115,8 @@ module.exports = class GameDataPlugin {
                 console.log("Writing to: " + PROJECT_FILE);
               }
               callback();
-            }
-          );
+            });
+          });
         }
       );
     });
