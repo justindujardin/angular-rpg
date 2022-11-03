1499c13d732ae66d7dfb8ba1e3abffcd4796e02b
Justin DuJardin <justin@dujardinconsulting.com>
2022-11-02 18:59:35 -0700

    test(temple-feature): assert temple close cases
     - should output onClose event when choosing not to heal
     - cleanup button clicking

--- src/app/routes/world/features/temple-feature.component.spec.ts
+++ src/app/routes/world/features/temple-feature.component.spec.ts
@@ -1,15 +1,22 @@
 import { TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 import { RouterTestingModule } from '@angular/router/testing';
 import { Store } from '@ngrx/store';
-import { map } from 'rxjs/operators';
+import * as Immutable from 'immutable';
+import { map, take } from 'rxjs/operators';
 import { APP_IMPORTS } from '../../../app.imports';
 import { AppState } from '../../../app.model';
 import { ITiledObject } from '../../../core/resources/tiled/tiled.model';
+import { IPartyMember } from '../../../models/base-entity';
+import { EntityAddBeingAction } from '../../../models/entity/entity.actions';
 import { EntityWithEquipment } from '../../../models/entity/entity.model';
-import { GameStateHurtPartyAction } from '../../../models/game-state/game-state.actions';
+import {
+  GameStateHurtPartyAction,
+  GameStateNewAction,
+} from '../../../models/game-state/game-state.actions';
+import { GameState } from '../../../models/game-state/game-state.model';
 import { getGamePartyGold, getGamePartyWithEquipment } from '../../../models/selectors';
 import { GameWorld } from '../../../services/game-world';
-import { RPGGame } from '../../../services/rpg-game';
 import {
   ITempleFeatureProperties,
   TempleFeatureComponent,
@@ -32,14 +39,20 @@ function getPartyWithEquipment(store: Store<AppState>): EntityWithEquipment[] {
   let newParty: EntityWithEquipment[] = [];
   store
     .select(getGamePartyWithEquipment)
-    .pipe(map((f) => f.toJS()))
+    .pipe(
+      map((f) => f.toJS()),
+      take(1)
+    )
     .subscribe((s) => (newParty = s));
   return newParty;
 }
 
 function getPartyGold(store: Store<AppState>): number {
   let partyGold = 0;
-  store.select(getGamePartyGold).subscribe((s) => (partyGold = s));
+  store
+    .select(getGamePartyGold)
+    .pipe(take(1))
+    .subscribe((s) => (partyGold = s));
   return partyGold;
 }
 
@@ -55,20 +68,56 @@ function damageParty(
 
 describe('TempleFeatureComponent', () => {
   let world: GameWorld;
+  const tileObject: any = {};
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [RouterTestingModule, ...APP_IMPORTS],
       declarations: [TempleFeatureComponent],
     }).compileComponents();
     world = TestBed.inject(GameWorld);
-    const game = TestBed.inject(RPGGame);
-    await game.initGame(false);
+    const warrior: IPartyMember = {
+      id: 'warrior-class-id',
+      eid: 'invalid-hero',
+      status: [],
+      type: 'warrior',
+      name: 'warrior',
+      level: 1,
+      exp: 0,
+      icon: '',
+      hp: 20,
+      maxhp: 20,
+      mp: 20,
+      maxmp: 20,
+      strength: [10],
+      agility: [10],
+      vitality: [10],
+      luck: [10],
+      hitpercent: [10],
+      intelligence: [10],
+      magicdefense: [10],
+    };
+    const initialState: GameState = {
+      party: Immutable.List<string>([warrior.eid]),
+      inventory: Immutable.List<string>(),
+      battleCounter: 0,
+      keyData: Immutable.Map<string, any>(),
+      gold: 100,
+      combatZone: '',
+      location: 'example',
+      position: { x: 12, y: 8 },
+      boardedShip: false,
+      shipPosition: { x: 7, y: 23 },
+    };
+    world.store.dispatch(new GameStateNewAction(initialState));
+    world.store.dispatch(new EntityAddBeingAction(warrior));
   });
 
   it('should heal party when resting', async () => {
     const fixture = TestBed.createComponent(TempleFeatureComponent);
     const comp: TempleFeatureComponent = fixture.componentInstance;
     comp.feature = TEMPLE_FEATURE;
+    fixture.detectChanges();
+    comp.enter(tileObject);
     fixture.detectChanges();
     const party = getPartyWithEquipment(world.store);
 
@@ -81,7 +130,8 @@ describe('TempleFeatureComponent', () => {
     // Assert members are damaged by 10
     getPartyWithEquipment(world.store).forEach((p) => expect(p.hp).toBe(p.maxhp - 10));
 
-    comp._onRest$.next();
+    const doRest = fixture.debugElement.query(By.css('.rest'));
+    doRest.triggerEventHandler('click');
 
     // Party at max health again
     getPartyWithEquipment(world.store).forEach((p) => expect(p.maxhp).toBe(p.hp));
@@ -91,6 +141,8 @@ describe('TempleFeatureComponent', () => {
     const comp: TempleFeatureComponent = fixture.componentInstance;
     comp.feature = { ...TEMPLE_FEATURE, properties: { cost: 200 } };
     fixture.detectChanges();
+    comp.enter(tileObject);
+    fixture.detectChanges();
     const party = getPartyWithEquipment(world.store);
 
     // Damage party
@@ -99,7 +151,8 @@ describe('TempleFeatureComponent', () => {
     // Assert members are damaged by 10
     getPartyWithEquipment(world.store).forEach((p) => expect(p.hp).toBe(p.maxhp - 10));
 
-    comp._onRest$.next();
+    const doRest = fixture.debugElement.query(By.css('.rest'));
+    doRest.triggerEventHandler('click');
 
     // Party still at damaged health
     getPartyWithEquipment(world.store).forEach((p) => expect(p.hp).toBe(p.maxhp - 10));
@@ -109,14 +162,34 @@ describe('TempleFeatureComponent', () => {
     const comp: TempleFeatureComponent = fixture.componentInstance;
     comp.feature = TEMPLE_FEATURE;
     fixture.detectChanges();
-    const party = getPartyWithEquipment(world.store);
+    comp.enter(tileObject);
+    fixture.detectChanges();
     // Party at full health
     getPartyWithEquipment(world.store).forEach((p) => expect(p.hp).toBe(p.maxhp));
     expect(getPartyGold(world.store)).toBe(100);
 
-    comp._onRest$.next();
+    const doRest = fixture.debugElement.query(By.css('.rest'));
+    doRest.triggerEventHandler('click');
 
     // Still at 100 gold
     expect(getPartyGold(world.store)).toBe(100);
+  });
+  it('should output onClose when choosing not to heal', async () => {
+    const fixture = TestBed.createComponent(TempleFeatureComponent);
+    const comp: TempleFeatureComponent = fixture.componentInstance;
+    comp.feature = TEMPLE_FEATURE;
+    fixture.detectChanges();
+    comp.enter(tileObject);
+    fixture.detectChanges();
+
+    expect(comp.active).toBe(true);
+
+    let clicked = false;
+    comp.onClose.pipe(take(1)).subscribe(() => (clicked = true));
+
+    const doClose = fixture.debugElement.query(By.css('.close'));
+    doClose.triggerEventHandler('click');
+
+    expect(clicked).toBe(true);
   });
 });
