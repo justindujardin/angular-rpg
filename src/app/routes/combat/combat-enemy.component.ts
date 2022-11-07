1c11a60366811ea8f5581c256eeb079407136ed7
Justin DuJardin <justin@dujardinconsulting.com>
2022-11-07 13:03:02 -0800

    fix(combat): CombatEnemyComponent null error on unload

--- src/app/routes/combat/combat-enemy.component.ts
+++ src/app/routes/combat/combat-enemy.component.ts
@@ -26,7 +26,7 @@ export class CombatEnemyComponent
 
   // @ts-ignore
   @Input() icon: string;
-  @Input() scene: Scene;
+  @Input() scene: Scene | null = null;
   @Input() combat: any; // CombatComponent - flirts with circular imports
 
   // @ts-ignore
@@ -45,7 +45,7 @@ export class CombatEnemyComponent
   private _spriteSubscription: Subscription | null;
 
   ngAfterViewInit(): void {
-    this.scene.addObject(this);
+    this.scene?.addObject(this);
     this.addBehavior(this.render);
     this.addBehavior(this.attack);
     this._spriteSubscription = this.model$
