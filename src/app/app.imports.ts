71a45060a852dde2f602f29b14473bff8e3d8a4f
Justin DuJardin <justin@dujardinconsulting.com>
2024-02-09 13:04:12 -0800

    chore: update ngrx/effects

--- src/app/app.imports.ts
+++ src/app/app.imports.ts
@@ -41,7 +41,7 @@ if (environment.useDevTools) {
       maxAge: 25, // Retains last 25 states
       logOnly: environment.production, // Restrict extension to log-only mode
       autoPause: true, // Pauses recording actions and state changes when the extension window is not open
-    }),
+     connectInZone: true}),
   ]);
 }
 
